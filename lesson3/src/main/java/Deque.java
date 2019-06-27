615f1a7a3a5685e8f9fa003be723c4d889964e0b
Mikhail <levinmk23@gmail.com>
2019-06-27 17:12:39 +0400

    lesson3 - с полвиной реализации класс Стэк

---
+++ lesson3/src/main/java/Deque.java
@@ -0,0 +1,61 @@
+public class Deque<T> implements TwoSizeQueue<T> {
+
+    private TwoLinkNode<T> front, back;
+    private int size;
+
+    @Override
+    public String toString() {
+        //TODO
+        return "";
+    }
+
+    @Override
+    public void addBack(T element) {
+        //TODO
+    }
+
+    @Override
+    public void addFront(T element) {
+        //TODO
+    }
+
+    @Override
+    public T popBack() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public T popFront() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public T back() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public T front() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void order() {
+        //TODO
+    }
+
+    @Override
+    public void reverseOrder() {
+        //TODO
+    }
+
+    @Override
+    public int size() {
+        //TODO
+        return 0;
+    }
+}
---
+++ lesson3/src/main/java/Queue.java
@@ -0,0 +1,39 @@
+public class Queue<T> implements Fifo<T> {
+
+    private OneLinkNode<T> head;
+    private int size;
+
+    @Override
+    public String toString() {
+        //TODO
+        return "";
+    }
+
+    @Override
+    public void push(T element) {
+        //TODO
+    }
+
+    @Override
+    public T pop() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public int size() {
+        //TODO
+        return 0;
+    }
+
+    @Override
+    public void order() {
+        //TODO
+    }
+}
