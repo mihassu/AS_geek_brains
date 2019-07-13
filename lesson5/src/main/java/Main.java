0ce7b174da5c5867ddecb3a13e9a1801be400d57
mihassu <mihassu1@gmail.com>
2019-07-13 14:37:26 +0300

    Уро5 ДЗ

--- lesson5/src/main/java/Main.java
+++ lesson5/src/main/java/Main.java
@@ -10,39 +10,99 @@ public class Main {
 
     public static void main(String[] args) {
         Main m = new Main();
+
+        RFunctions functions = new RFunctions();
+
+        //Фибоначчи
+        BigInteger fib = functions.fib(8);
+        System.out.println("fib(8): " + fib.toString());
+
+        //Факториал
+        BigInteger fac = functions.factor(7);
+        System.out.println("fac(7): " + fac.toString());
+
+//        //числа в обратном порядке
+//        functions.reverse(new Scanner(System.in), new PrintWriter(System.out, true));
+
+//        //Список пополам
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(7);
+        list.add(2);
+        list.add(5);
+        list.add(1);
+        list.add(4);
+        list.add(15);
+        list.add(8);
+        list.add(9);
+        list.add(3);
+        list.add(0);
+
+//        System.out.println("list:" + list);
+//        Pair<ArrayList<Integer>, ArrayList<Integer>> pair = functions.split(list);
+//        System.out.print("split(list) 1: ");
+//        System.out.println(pair.getKey());
+//        System.out.print("split(list) 2: ");
+//        System.out.println(pair.getValue());
+//
+//        //Вернуть отсортированный список, сливаемый из двух отсортированных
+//        ArrayList<Integer> sortList1 = new ArrayList<>();
+//        sortList1.add(1);
+//        sortList1.add(3);
+//        sortList1.add(7);
+//        sortList1.add(10);
+//        sortList1.add(12);
+//        ArrayList<Integer> sortList2 = new ArrayList<>();
+//        sortList2.add(0);
+//        sortList2.add(4);
+//        sortList2.add(6);
+//        sortList2.add(9);
+//        sortList2.add(15);
+//        System.out.println(functions.merge(sortList1, sortList2));
+
+        //Сортировка слиянием
+        System.out.println("Массив: " + list);
+        System.out.print("Сортировка слиянием: ");
+        System.out.println(functions.mergeSort(list));
+
+        //максимальное число не превышающее лимит
+        int[] data = {5, 2, 1, 7, 4, 8, 3};
+        System.out.println("max: " + functions.knackPack(data, 3));
+
+
     }
 
-    BigInteger fib(int n){
-        return null;
-    }
 
-    BigInteger factor(int n){
-        return null;
-    }
-
-    void reverse(Scanner in, PrintWriter out){
-
-    }
-
-    void mergeSort(ArrayList<Integer> list){
-
-    }
-
-    Pair<ArrayList<Integer>, ArrayList<Integer>> split(ArrayList<Integer> list){
-
-        return null;
-    }
-
-    ArrayList<Integer> merge(ArrayList<Integer> left, ArrayList<Integer> right){
-
-        return null;
-    }
-
-    void permutation(int [] data, int n, int k, int index){
-
-    }
-
-    long knackPack(int [] data, long limit, long  start, int index, ArrayList<Integer> list){
-        return 0;
-    }
+//    BigInteger fib(int n){
+//        return null;
+//    }
+//
+//    BigInteger factor(int n){
+//        return null;
+//    }
+//
+//    void reverse(Scanner in, PrintWriter out){
+//
+//    }
+//
+//    void mergeSort(ArrayList<Integer> list){
+//
+//    }
+//
+//    Pair<ArrayList<Integer>, ArrayList<Integer>> split(ArrayList<Integer> list){
+//
+//        return null;
+//    }
+//
+//    ArrayList<Integer> merge(ArrayList<Integer> left, ArrayList<Integer> right){
+//
+//        return null;
+//    }
+//
+//    void permutation(int [] data, int n, int k, int index){
+//
+//    }
+//
+//    long knackPack(int [] data, long limit, long  start, int index, ArrayList<Integer> list){
+//        return 0;
+//    }
 }
--- lesson5/src/main/java/RFunctions.java
+++ lesson5/src/main/java/RFunctions.java
@@ -4,62 +4,161 @@ import java.io.InputStream;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RFunctions {
 
+    private BigInteger[] memo = new BigInteger[1000];
+    private List<Integer> stackArr = new LinkedList<>();
+
+
     //функция должна находить число Фибоначчи с номер н
-    BigInteger fib(int n){
-        //TODO
-        return null;
+    BigInteger fib(int n) {
+
+        if (n == 1) {
+            return new BigInteger("0");
+        }
+        if (n == 2) {
+            return new BigInteger("1");
+        }
+
+        if (memo[n] != null) {
+            return memo[n];
+        }
+        memo[n] = fib(n - 1).add(fib(n - 2));
+        return memo[n];
     }
 
     //функция должна считать факториал числа
-    BigInteger factor(int n){
-        //TODO
-        return null;
+    BigInteger factor(int n) {
+
+        BigInteger result;
+        BigInteger nBig = new BigInteger(n + "");
+
+        if (n == 1) {
+            return new BigInteger("1");
+        }
+        result = nBig.multiply(factor(n - 1));
+
+        return result;
     }
 
     //функция должна вводить числа из in до тех пор пока не будет введен 0
     //и вывести в out все числа в обратном порядке без 0
-    void reverse(Scanner in, PrintWriter out){
-        //TODO
+    void reverse(Scanner in, PrintWriter out) {
+        int n;
+        n = in.nextInt();
+        if (n == 0) {
+            out.println(stackArr.toString());
+            return;
+        }
+        ((LinkedList<Integer>) stackArr).addFirst(n);
+        out.println(n);
+
+        reverse(in, out);
+
+        //        while (true){
+//            n = in.nextInt();
+//            if (n == 0) {
+//                System.out.println(stackArr.toString());
+//                break;
+//            }
+//            ((LinkedList<Integer>) stackArr).addFirst(n);
+//            out.println(((LinkedList<Integer>) stackArr).peekFirst());
+//        }
     }
+
+//    //функция должна отсортировать массив слиянием
+//    ArrayList<Integer> mergeSort(ArrayList<Integer> list) {
+//
+//        sortingArr = list;
+//        sortedArr = new ArrayList<>();
+//
+//        mergeSortRec(sortedArr, 0, list.size());
+//        return sortedArr;
+//
+//    }
 
     //функция должна отсортировать массив слиянием
-    ArrayList<Integer> mergeSort(ArrayList<Integer> list){
-        //TODO
-        return null;
+    ArrayList<Integer> mergeSort(ArrayList<Integer> list) {
+
+        if(list.size() == 1 || list.size() == 0) {return list;}
+
+        return merge(mergeSort(split(list).getKey()), mergeSort(split(list).getValue()));
     }
 
+
     //функция должна разбить список пополам (2 2 для 4 или 3 2 для 5)
-    Pair<ArrayList<Integer>, ArrayList<Integer>> split(ArrayList<Integer> list){
-        //TODO
-        return null;
+    Pair<ArrayList<Integer>, ArrayList<Integer>> split(ArrayList<Integer> list) {
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list1.addAll(list); //копируем list, чтобы не изменять его
+
+        int len = list1.size();
+        for (int i = len - 1; i >= len / 2; i--) {
+            list2.add(list1.remove(i));
+        }
+        return new Pair<>(list1, list2);
     }
 
     //функция должна вернуть отсортированный список, сливаемый из двух отсортированных
-    ArrayList<Integer> merge(ArrayList<Integer> left, ArrayList<Integer> right){
-        //TODO
-        return null;
+    ArrayList<Integer> merge(ArrayList<Integer> left, ArrayList<Integer> right) {
+        ArrayList<Integer> mergedList = new ArrayList<>();
+        int l = 0, r = 0;
+
+        /*берем один массив и начинаем сравнивать его элементы с другим. Чйе элемент меньше - записаваем его в
+        третий масси и итерируемся (итерируемся только по тому массиву, из которого переносим элемент!)*/
+        while (l < left.size() && r < right.size()) {
+            if (left.get(l) < right.get(r)) {
+                mergedList.add(left.get(l));
+                l++;
+            } else {
+                mergedList.add(right.get(r));
+                r++;
+            }
+        }
+        // переносим оставшиеся в ОДНОМ из массивов элементы
+        while (l < left.size()) {
+            mergedList.add(left.get(l));
+            l++;
+        }
+        while (r < right.size()) {
+            mergedList.add(right.get(r));
+            r++;
+        }
+        return mergedList;
     }
 
     //функция должна вернуть К-ую перестановку n-элементного множества
     //уникальных целых чисел дата
-    int [] permutation(int [] data, int n, int k, int index, PrintWriter out){
+    int[] permutation(int[] data, int n, int k, int index, PrintWriter out) {
         //TODO
         return null;
     }
 
     //функция должна вернуть максимальную массу из предметов в дата, но не превышающую
     //лимит
-    long knackPack(int [] data, long limit){
-        return knackPack(data, limit, limit, 0);
+    long knackPack(int[] data, long limit) {
+        ArrayList<Integer> sortData = new ArrayList<>();
+        for (int o: data) {
+            sortData.add(o);
+        }
+
+        return knackPack(mergeSort(sortData), limit, limit, 0);
     }
 
-    private long knackPack(int [] data, long limit, long start, int index){
-        //TODO
-        return 0;
+    private long knackPack(ArrayList<Integer> sortData, long limit, long start, int index) {
+
+
+        if (index >= sortData.size() - 1) {return 0;}
+        if (sortData.get(index) >= limit) {return sortData.get(index - 1);}
+
+        return knackPack(sortData, limit, start, index + 1);
+
+
     }
 
 }
